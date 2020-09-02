41607dbb8e03ce8f5deb5617d2c33e1ce144a6fc
Pablo Coello <pablo.coellopulido@outlook.es>
2020-09-02 13:51:12 +0200

    Update

--- visualization.py
+++ visualization.py
@@ -1,126 +1,94 @@
 import pandas as pd
 import geopandas as gpd
 import numpy as np
-from geopandas.tools import sjoin
 import folium
 from folium.plugins import MarkerCluster
 from folium import IFrame
 import shapely
 from shapely.geometry import Point
-import unicodedata
-import pysal as ps
-import mapclassify
 import branca
+import json
 
 
 class visualization():
     '''
     '''
 
-    def __init__(self, ut, specific_conf, var_dict, threats, style='OpenStreetMap'):
+    def __init__(self):
         '''
         '''
-        self.starting_point = self.get_starting_point(ut)
+
+        with open('./conf.json', 'r') as f:
+            self.conf = json.load(f)
+
+        self.starting_point = self.get_starting_point()
         mapa = self.base_map(style)
-        for threat in threats.keys():
-            print(threat)
-            self.add_threat_map(mapa, threat, threats)
-            
-        for var in var_dict.keys():
-            print(var)
-            mapa = self.add_var_map(mapa, var_dict, specific_conf, layer=var)
+
+        for layer in self.conf["info_layers"].keys():
+            print(layer)
+            self.add_layer_map(mapa, layer)
 
         self.layer = mapa
         folium.LayerControl().add_to(self.layer)
 
+
     def to_html(self):
-        self.layer.save('output.html')
+        self.layer.save(self.conf["output_path"])
 
-    def base_map(self, style='OpenStreetMap'):
-        '''
-        '''
-        toret = folium.Map(self.starting_point, zoom_start=12, tiles=style)
-        return toret
-
-    def add_threat_map(self, mapa, threat, threats):
-        '''
-        '''
-        if len(threats[threat]['val'].unique()) > 1:
-            colorscale = branca.colormap.linear.YlOrRd_09.scale(
-                threats[threat]['val'].min(), threats['erosion']['val'].max())
-            colorscale = colorscale.to_step(
-                n=len(threats[threat]['val'].unique()))
-
-        folium.GeoJson(threats[threat],
-                       name=threat,
-                       style_function=lambda x: {
-            'weight': 1,
-            'color': '#545453',
-            'fillColor': '#9B9B9B' if len(threats[threat]['val'].unique()) == 1
-            else colorscale(x['properties']['val']),
-            'fillOpacity': 0.2
-        },
-            highlight_function=lambda x: {
-            'weight': 3,
-            'color': 'black',
-            'fillOpacity': 1
-        },
-            tooltip=folium.features.GeoJsonTooltip(
-            fields=['val'],
-            aliases=[threat + 'risk']
-        )).add_to(mapa)
-        
     
-    def add_markers(self, gdf, mapa, specific_conf, layer):
-        width, height = 310,110
-        popups, locations = [], []
-        for idx, row in gdf.iterrows():
-            try:
-                locations.append(sum([[p.y, p.x] for p in row.geom], []))
-            except:
-                locations.append([row['geom'].y, row['geom'].x])
-            variable = row[specific_conf['var_list'][layer]['variables']]
-            name = specific_conf['var_list'][layer]['name']
-            iframe = self.poptext(variable, name)
-            popups.append(iframe)
-            
-        h = folium.FeatureGroup(name=specific_conf['var_list'][layer]['name'])
-        h.add_children(MarkerCluster(locations=locations, popups=popups)).add_to(mapa)
-        return mapa
-
-    def poptext(self, variable, name):
-        return "<a href=\"" + name + "\">" + variable  + "</a>"
-    
-    def add_var_map(self, mapa, var, specific_conf, layer):
-        features = ['erosion_val', 'debrisflow_val', 'landslide_val']
-        features.append(specific_conf['var_list'][layer]['variables'])
-        if specific_conf['var_list'][layer]['type'] == 'lines':
-            folium.Choropleth(
-                var[layer][var[layer].geometry.length > 0.001],
-                data=var[layer],
-                columns=features,
-                line_weight=1,
-                line_color='black',
-                legend_name=specific_conf['var_list'][layer]['name']
-            ).add_to(mapa)
-        else:
-            mapa = self.add_markers(
-                gdf=var[layer], 
-                mapa=mapa,
-                specific_conf=specific_conf, 
-                layer=layer
-                )
-        return mapa
-
-    def get_starting_point(self, ut):
+    def get_starting_point(self):
         '''
         '''
-        starting_point = ut.get_gdf_from_SQL('burned_area').centroid
+        starting_point = gpd.read_file(self.conf["main_layer_path"]).centroid
         return [float(starting_point.y), float(starting_point.x)]
 
+
+    def base_map(self):
+        '''
+        '''
+        toret = folium.Map(
+            self.starting_point, 
+            zoom_start=self.conf["zoom"],
+            tiles=self.conf["style"])
+        return toret
+
+
+    def add_layer_map(self, mapa, layer):
+        '''
+        '''
+        layer_gdf = gpd.read_file(self.conf['info_layers'][layer]['path'])
+        val = self.conf['info_layers'][layer]['val_variable']
+        layer_conf = self.conf['info_layers'][layer]
+
+        if len(layer_gdf[val].unique()) > 1:
+            colorscale = branca.colormap.linear.YlOrRd_09.scale(
+                layer_gdf[val].min(), layer_gdf[val].max()
+            )
+            colorscale = colorscale.to_step(
+                n=len(layer_gdf[val].unique())
+            )
+
+        folium.GeoJson(
+            layer_gdf,
+            name=layer,
+            style_function=lambda x: {
+                'weight': layer_conf['style_function']['weight'],
+                'color': layer_conf['style_function']['color'],
+                'fillColor': layer_conf['style_function']['fillColor'] if len(layer_gdf[val].unique()) == 1
+                else colorscale(x['properties'][val]),
+                'fillOpacity': layer_conf['style_function']['fillOpacity']
+            },
+            highlight_function=lambda x: {
+                'weight': layer_conf['highlight_function']['weight'],
+                'color': layer_conf['highlight_function']['color'],
+                'fillOpacity': layer_conf['highlight_function']['fillOpacity']
+            },
+            tooltip=folium.features.GeoJsonTooltip(
+                fields=self.conf['info_layers'][layer]['values_to_display'],
+                aliases=[layer]
+            )).add_to(mapa)
+
+
 if __name__ == '__main__':
-    pl = visualization(ut, specific_conf, var_dict=var,
-                    threats=threats)  # , style='Stamen Toner')
-    base = pl.base_map()
-    pl.layer
+    pl = visualization()
     pl.to_html()
